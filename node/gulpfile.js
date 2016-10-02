5c4a8f019d791290be688d896a3c51b9b3db8c4c
Deltax9k <deltax9000@163.com>
2016-10-02 11:11:07 +0800

    测试

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -3,5 +3,34 @@ const jshint = require('gulp-jshint')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const rename = require('gulp-rename')
+const path = require('path')
 
-gulp.task('')
+gulp.task('lint', () => {
+    gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+})
+
+gulp.task('less', () => {
+    return gulp.src('./less/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('./public'))
+})
+
+gulp.task('scripts', () => {
+    gulp.src('./js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('default', () => {
+    gulp.run('lint', 'sass', 'scripts')
+    gulp.watch('./js/*.js', () => {
+        gulp.run('lint', 'sass', 'scripts')
+    })
+})
